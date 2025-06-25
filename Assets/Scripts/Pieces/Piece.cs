b06b880fac96e47d08294b0e2beed701818ec44c
Daniel Liang <dawgswirly@gmail.com>
2025-06-24 22:13:26 -0600

    Create abstract class for pieces

--- Assets/Scripts/Pieces/Piece.cs
+++ Assets/Scripts/Pieces/Piece.cs
@@ -1,16 +1,46 @@
+using System.Collections.Generic;
 using UnityEngine;
 
-public class Piece : MonoBehaviour
+public abstract class Piece : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public enum Alignment
     {
-        
+        Neutral,
+        White,
+        Black
+    }
+    
+    [SerializeField] private Tile tile;
+    [SerializeField] private Alignment alignment;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    protected void Start()
+    {
+        transform.position = tile.transform.position;
     }
 
     // Update is called once per frame
-    void Update()
+    protected abstract void Update();
+
+    public abstract List<Vector2Int> LegalMoves();
+
+    public bool CanBeMoved(Alignment alignmentMoving)
     {
+        if (alignment == Alignment.Neutral)
+        {
+            return false;
+        }
         
+        return (alignmentMoving == alignment); 
+    }
+
+    public bool CanBeCaptured(Alignment alignmentCapturing)
+    {
+        if (alignmentCapturing == Alignment.Neutral || alignment == Alignment.Neutral)
+        {
+            return false;
+        }
+        
+        return (alignmentCapturing != alignment);
     }
 }
