642150c737d66e809749f60d543cda5b681184d0
Daniel Liang <dawgswirly@gmail.com>
2025-07-01 20:03:38 -0600

    Create SovereignPieceSO variants using new sprites

--- Assets/Scripts/ScriptableObjects/SovereignPieceSO.cs
+++ Assets/Scripts/ScriptableObjects/SovereignPieceSO.cs
@@ -3,6 +3,11 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "SovereignPiece", menuName = "ScriptableObjects/Sovereign Piece")]
 public class SovereignPieceSO : ScriptableObject
 {
-    public Color color;
     public int id;
+    public Sprite kingSprite;
+    public Sprite queenSprite;
+    public Sprite bishopSprite;
+    public Sprite knightSprite;
+    public Sprite rookSprite;
+    public Sprite pawnSprite;
 }
